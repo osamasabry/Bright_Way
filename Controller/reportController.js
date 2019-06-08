216a603e8532d0382310dd8406b3f3cafc1b1b8c
osamasabry <osamasabry14@gmail.com>
2019-06-08 20:03:50 +0200

    fix report

--- Controller/reportController.js
+++ Controller/reportController.js
@@ -221,51 +221,51 @@ module.exports = {
 	    	})
 	},
 
-	getRoomingList:function (request,response){
-		// var date1 = new Date('2019-02-15');
-		// var date2 = new Date('2019-02-18');
+	// RoomingList:function (request,response){
+	// 	// var date1 = new Date('2019-02-15');
+	// 	// var date2 = new Date('2019-02-18');
 
-		var date1 = new Date(request.body.From);
-		var date2 = new Date(request.body.To);
+	// 	var date1 = new Date(request.body.From);
+	// 	var date2 = new Date(request.body.To);
 
-		RoomBusy.aggregate([
-				{$match: { 
-					$and:[
-							{RoomBusy_HotelID:Number(request.body.RoomBusy_HotelID)},
-							{RoomBusy_Date:{ $gte: date1, $lte: date2}},
-						]
-				}},
-				{ $group: { _id : 
-							{	 	
-									 RoomBusy_Room_View_Code: '$RoomBusy_Room_View_Code', 
-									 RoomBusy_Room_Type_Code: '$RoomBusy_Room_Type_Code', 
-									 RoomBusy_Room_Count: '$RoomBusy_Room_Count', 
-									 RoomBusy_Reservation_Code: '$RoomBusy_Reservation_Code',
-									 RoomBusy_Note: '$RoomBusy_Note' 
-							 },
-							 RoomBusy_Room_Type_Code: {$first : '$RoomBusy_Room_Type_Code'}, 
-							 RoomBusy_Room_View_Code: {$first:'$RoomBusy_Room_View_Code'}, 
-							 RoomBusy_Reservation_Code :{$first:'$RoomBusy_Reservation_Code' }
-						}
-				}
-			])
-			.exec(function(err, roomBusy) {
-			    if (err){
-		    		response.send({message: err});
-				}
-		        if (roomBusy.length > 0) {
-		        	RoomBusy.populate(roomBusy, { path: 'Reservation'}, function(err, reserv) {
-		        		RoomBusy.populate(reserv, { path: 'RoomType'}, function(err, type) {
-		        			RoomBusy.populate(type, { path: 'RoomView'}, function(err, view) {
-		    					response.send(view);
-			        		});
-			        	});
-			        });
-		        }else{
-		    		response.send({message: 'Not Busy Room'});
-		        }
-	    	})
-	},
+	// 	RoomBusy.aggregate([
+	// 			{$match: { 
+	// 				$and:[
+	// 						{RoomBusy_HotelID:Number(request.body.RoomBusy_HotelID)},
+	// 						{RoomBusy_Date:{ $gte: date1, $lte: date2}},
+	// 					]
+	// 			}},
+	// 			{ $group: { _id : 
+	// 						{	 	
+	// 								 RoomBusy_Room_View_Code: '$RoomBusy_Room_View_Code', 
+	// 								 RoomBusy_Room_Type_Code: '$RoomBusy_Room_Type_Code', 
+	// 								 RoomBusy_Room_Count: '$RoomBusy_Room_Count', 
+	// 								 RoomBusy_Reservation_Code: '$RoomBusy_Reservation_Code',
+	// 								 RoomBusy_Note: '$RoomBusy_Note' 
+	// 						 },
+	// 						 RoomBusy_Room_Type_Code: {$first : '$RoomBusy_Room_Type_Code'}, 
+	// 						 RoomBusy_Room_View_Code: {$first:'$RoomBusy_Room_View_Code'}, 
+	// 						 RoomBusy_Reservation_Code :{$first:'$RoomBusy_Reservation_Code' }
+	// 					}
+	// 			}
+	// 		])
+	// 		.exec(function(err, roomBusy) {
+	// 		    if (err){
+	// 	    		response.send({message: err});
+	// 			}
+	// 	        if (roomBusy.length > 0) {
+	// 	        	RoomBusy.populate(roomBusy, { path: 'Reservation'}, function(err, reserv) {
+	// 	        		RoomBusy.populate(reserv, { path: 'RoomType'}, function(err, type) {
+	// 	        			RoomBusy.populate(type, { path: 'RoomView'}, function(err, view) {
+	// 	    					response.send(view);
+	// 		        		});
+	// 		        	});
+	// 		        });
+	// 	        }else{
+	// 	    		response.send({message: 'Not Busy Room'});
+	// 	        }
+	//     	})
+	// },
 
 	getReservationDetails:function (request,response){
 		// var date1 = new Date('2019-02-15');
@@ -323,6 +323,59 @@ module.exports = {
 			        });
 		        }else{
 		    		response.send({message: 'Not Reservation Room'});
+		        }
+	    	})
+	},
+
+	getRoomingList:function (request,response){
+		// var date1 = new Date('2019-06-05');
+		// var date2 = new Date('2019-06-08');
+
+		var date1 = new Date(request.body.From);
+		var date2 = new Date(request.body.To);
+
+		Reservation.aggregate([
+				{$match: { 
+					$and:[
+							{Reservation_Hotel_ID:Number(request.body.Reservation_Hotel_ID)},
+							{Reservation_Date_From:{ $gte: date1},Reservation_Date_To: {$lte: date2}},
+						]
+				}},
+				{$unwind: "$Reservation_Room" },
+				{ $group: { _id : 
+							{	 	
+									 Reservation_View_Code: '$Reservation_Room.View', 
+									 Reservation_Type_Code: '$Reservation_Room.Type', 
+									 RoomBusy_Room_Count: '$Reservation_Room.Count', 
+									 Reservation_Customer_ID: '$Reservation_Customer_ID',
+									 Reservation_Note: '$Reservation_Note' 
+							 },
+							 // RoomBusy_Room_Type_Code: {$first : '$RoomBusy_Room_Type_Code'}, 
+							 // RoomBusy_Room_View_Code: {$first:'$RoomBusy_Room_View_Code'}, 
+						}
+				},
+				{
+		        "$project": {
+			            "Reservation_Room.View": "$_id.Reservation_View_Code",
+			            "Reservation_Room.Type": "$_id.Reservation_Type_Code",
+			            "Reservation_Customer_ID":"$_id.Reservation_Customer_ID",
+			        }		
+			    },
+			])
+			.exec(function(err, reserve) {
+			    if (err){
+		    		response.send({message: err});
+				}
+		        if (reserve.length > 0) {
+		        		Reservation.populate(reserve, { path: 'RoomType'}, function(err, type) {
+		        			Reservation.populate(type, { path: 'RoomView'}, function(err, view) {
+		    					Reservation.populate(view, { path: 'Customer'}, function(err, customer) {
+		    						response.send(customer);
+			        			});
+			        		});
+			        	});
+		        }else{
+		    		response.send({message: 'Not Reserve Room In This Date'});
 		        }
 	    	})
 	},
